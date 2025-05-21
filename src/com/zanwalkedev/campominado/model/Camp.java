79586b7731ee8ea680b223444097e44879d88b06
Zan Rocha <andre.rocha2302@gmail.com>
2025-05-21 12:46:51 -0300

    update: camp attributes and List set done

--- src/com/zanwalkedev/campominado/model/Camp.java
+++ src/com/zanwalkedev/campominado/model/Camp.java
@@ -1,4 +1,21 @@
 package com.zanwalkedev.campominado.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Camp {
+
+    private final int line;
+    private final int column;
+
+    private boolean mined; //false por padrão
+    private boolean opened; //false por padrão
+    private boolean marked;
+
+    private List<Camp> adjacent = new ArrayList<>();
+
+    Camp(int line, int column){
+        this.line = line;
+        this.column = column;
+    }
 }
