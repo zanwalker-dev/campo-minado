56bad9ef55d3720833d043110d02d8607a5e607f
Zan Rocha <andre.rocha2302@gmail.com>
2025-05-22 16:47:27 -0300

    feat: table methods to generate and associate the camps

--- src/com/zanwalkerdev/campominado/model/Board.java
+++ src/com/zanwalkerdev/campominado/model/Board.java
@@ -1,4 +1,44 @@
 package com.zanwalkerdev.campominado.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Board {
+
+    private int lines;
+    private int columns;
+    private int mines;
+
+    private final List<Camp> camps = new ArrayList<>();
+
+    public Board(int lines, int columns, int mines) {
+        this.lines = lines;
+        this.columns = columns;
+        this.mines = mines;
+
+        generateCamps();
+        associateAdjacents();
+        drawMines();
+    }
+
+    private void generateCamps() {
+        for (int line = 0; line < this.lines; line++) {
+            for (int column = 0; column < this.columns; column++) {
+                camps.add(new Camp(line, column));
+            }
+        }
+    }
+
+    private void associateAdjacents() {
+        for(Camp c1: camps) {
+            for(Camp c2: camps) {
+                c1.addAdjacent(c2);
+            }
+        }
+    }
+
+    private void drawMines() {
+    }
+
 }
